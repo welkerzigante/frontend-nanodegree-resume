a1be992aef8469aa4de9fd26e4f8c5045cd13056
welkerzigante <welker.zigante@gmail.com>
2017-02-21 16:42:17 -0300

    .replace

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,8 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var name = "Welker Zigante"
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = "Programador Web";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
